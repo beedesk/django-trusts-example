87b49b3d44a094c430f4ddfbaba101456055a10c
Thomas @ BeeDesk <thomasleaf@gmail.com>
2016-04-24 10:24:05 -0700

    Changed to use git version (instead of release) of trusts. Made very minor fix / clean up.

--- bin/manage.sh
+++ bin/manage.sh
@@ -2,7 +2,7 @@
 set -e
 virtualenv venv/
 source ./venv/bin/activate
-pip freeze | grep -v -f requirements.txt - | grep -v '^#' | xargs -r pip uninstall -y
+pip freeze | grep -v -f requirements.txt - | grep -v '^#' | xargs pip uninstall -y
 pip install -r requirements.txt
 source ./bin/localenv.sh
 python apps/manage.py $@
